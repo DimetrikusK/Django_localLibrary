d291d205159a1f05ae37cb94c87abb65e5900fd2
dimetrikusk <dimetrikusk@gmail.com>
2021-04-24 23:56:02 +0300

    Added files and changes required for deployment to heroku

--- Django_locallibrary/settings.py
+++ Django_locallibrary/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'Django_locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'locallibrary',
-#         'USER': 'postgres',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'locallibrary',
+        'USER': 'postgres',
+        'PASSWORD': 'Oracle33',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
